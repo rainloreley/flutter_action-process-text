5a1646016e1716007158f0b539dcddf7ff6d05ee
Adrian Baumgart <adrian@abmgrt.dev>
2024-03-31 19:48:50 +0200

    migrated to v2

--- android/src/main/kotlin/com/example/action_process_text/ActionProcessTextPlugin.kt
+++ android/src/main/kotlin/com/example/action_process_text/ActionProcessTextPlugin.kt
@@ -1,74 +1,35 @@
 package com.example.action_process_text
 
-import android.content.Intent
-import android.os.Bundle
+import androidx.annotation.NonNull
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
 
+/** APlugin */
+class ActionProcessTextPlugin: FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
 
-/** ActionProcessTextPlugin */
-public class ActionProcessTextPlugin : io.flutter.app.FlutterActivity() {
-
-  companion object {
-    @JvmStatic
-    fun registerWith(registrarFor: PluginRegistry.Registrar) {
-
-    }
-
-    const val CHANNEL = "action_process_text"
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "a")
+    channel.setMethodCallHandler(this)
   }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    val channel = MethodChannel(flutterView, CHANNEL)
-    if (intent.action == Intent.ACTION_PROCESS_TEXT) {
-      val selectedText = intent.getStringExtra(Intent.EXTRA_PROCESS_TEXT) ?: ""
-      channel.invokeMethod("copiedText", selectedText)
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
     }
+  }
 
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
   }
 }
-
-
-
-//public class ActionProcessTextPlugin: FlutterPlugin, ActivityAware {
-//  /// The MethodChannel that will the communication between Flutter and native Android
-//  ///
-//  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-//  /// when the Flutter Engine is detached from the Activity
-//  private lateinit var channel : MethodChannel
-//
-//  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-//    channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "action_process_text")
-//    //channel.setMethodCallHandler(this);
-//
-//  }
-//  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-//    channel.setMethodCallHandler(null)
-//  }
-//
-//  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-//    print("onCreate called")
-//    if (intent.action == Intent.ACTION_PROCESS_TEXT) {
-//
-//      val selectedText = intent.getStringExtra(Intent.EXTRA_PROCESS_TEXT) ?: ""
-//
-//      channel.invokeMethod("copiedText", selectedText)
-//
-//
-//    }
-//  }
-//
-//  override fun onDetachedFromActivityForConfigChanges() {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-//    TODO("Not yet implemented")
-//  }
-//
-//  override fun onDetachedFromActivity() {
-//    TODO("Not yet implemented")
-//  }
-//
-//}
--- example/android/app/src/main/kotlin/com/example/action_process_text_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.action_process_text_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
